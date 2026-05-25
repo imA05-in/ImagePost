91c77a4e19d0f3f8f2f65120be3ba3c9d50175cc
Abhishek <abhishek29112005@gmail.com>
2026-05-25 16:36:00 +0530

    index for pages i/o

---
+++ src/pages/indexP.js
@@ -0,0 +1,7 @@
+import HomeP from "./HomeP";
+import LoginP from "./LoginP";
+import SingUpP from "./SignUpP";
+import AddPostP from "./AddPostP";
+import LogoutP from "./LogoutP";
+import AllPosts from "./AllPost";
+export {HomeP,LoginP, SingUpP, AddPostP, LogoutP, AllPosts}
