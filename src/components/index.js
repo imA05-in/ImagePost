e01e3507a63cf1ea209526440e2c40e1f77c5137
Abhishek <abhishek29112005@gmail.com>
2026-05-27 11:11:08 +0530

    import/export Signup, Login, Input, Container

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-
-export {Header, Footer}
+import Signup from "./Signup";
+import Container from "./Container";
+import Input from "./Input";
+import Login from "./Login";
+export {Header, Footer, Signup, Container,Input, Login}
