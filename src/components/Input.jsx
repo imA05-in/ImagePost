5e676ace6807497356bbacc14bca5c16970142d6
Abhishek <abhishek29112005@gmail.com>
2026-05-28 10:41:44 +0530

    fix ui

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,7 +2,7 @@ import { ID } from "appwrite";
 
 export default function Input({type="text",label,ref,...props}){
  return(
-    <div className="border rounded p-1 px-2 flex w-full justify-center items-center">
+    <div className="border rounded p-1 px-2 flex w-full items-center">
     <input type={type} id={ID.unique()} {...props} ref={ref} className={`focus:outline-none`} placeholder={label}/>
     </div>
  )
