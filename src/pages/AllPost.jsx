50bd14acf2ea61f4b89d2a4c654506ee4f1fb922
Abhishek <abhishek29112005@gmail.com>
2026-05-30 13:26:04 +0530

    AddPost component

--- src/pages/AllPost.jsx
+++ src/pages/AllPost.jsx
@@ -1,7 +1,30 @@
-export default function AllPosts(){
-    return(
-        <div>
-            AllPosts
-        </div>
-    )
+//todo: add public/private tag to posts and show private posts(in AllPosts section) only to the author
+import { Post } from "../components/index";
+import appwriteService from "../appwrite/Config";
+import { useEffect, useState } from "react";
+import { PostCard } from "../components/index";
+
+export default function AllPosts() {
+  const [Posts, setPosts] = useState([]);
+
+  useEffect(()=>{
+      appwriteService
+      .listRows()
+      .then((posts) => (setPosts(posts.rows)))
+      .catch((error) =>
+        console.log("AllPosts :: appwriteService.listRows: ", error),
+    );
+},[])
+
+  return (
+    <div className="flex flex-col md:flex-row md:justify-center items-center gap-4 py-2 flex-wrap">
+      {Posts.length > 0
+        ? Posts.map((post) => (
+            <div key={post.title}>
+              <PostCard post={post} />
+            </div>
+          ))
+        : null}
+    </div>
+  );
 }
