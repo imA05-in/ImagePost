16b3c4449d83e1076a9897766b35571a17467351
Abhishek <abhishek29112005@gmail.com>
2026-05-30 14:25:11 +0530

    remove strictmode

--- src/main.jsx
+++ src/main.jsx
@@ -63,10 +63,10 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  
     <Provider store={store}>
       <RouterProvider router={router} />
       {/* <App /> */}
     </Provider>
-  </StrictMode>,
+  
 );
