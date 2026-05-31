9f81190bddae59f72d64b4e2b4a875c273f7674c
Abhishek <abhishek29112005@gmail.com>
2026-05-31 08:48:01 +0530

    display error

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,14 +4,17 @@ import { useNavigate } from "react-router"
 import { useDispatch } from "react-redux"
 import authService from "../appwrite/Auth"
 import { toggleLoginRedux } from "../store/AuthSlice"
+import { useState } from "react"
 
 export default function Login(){
     
-    const {register, handleSubmit} = useForm()
+    const {register, handleSubmit, formState:{errors}} = useForm()
     const navigate = useNavigate()
     const dispatch = useDispatch()
+    const [err, setErr] = useState("")
     async function submit(data){
         try {
+            setErr("")
             authService.createEPSession({email: data.email, password: data.password})
             const userData = await authService.getAccount()
             if(userData){
@@ -21,6 +24,7 @@ export default function Login(){
                 navigate("/home")
             }
         } catch (error) {
+            setErr(error.message)
             console.log("/src/components/login.jsx: ", error);
             
         }
@@ -28,9 +32,18 @@ export default function Login(){
     
     return(
         <div >
+            {err && <p className="text-red-500" >{err}</p> }
             <form onSubmit={handleSubmit(submit)} className="flex flex-col gap-10 border rounded justify-center items-center w-full p-2 m-4 py-6" >
-            <Input label={`username`} type="text" {...register("email",{required:true})}/>
-            <Input label={`password`} type="password"{...register("password",{required:true})}/>
+                {errors.email && <p className="text-red-500 ">{errors.email.message}</p> }
+            <Input label={`username`} type="text" {...register("email",{required:true, pattern:{
+                 value:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                 message:"Invalid email format"
+            }})}/>
+            {errors.password && <p className="text-red-500" >{errors.password.message}</p> }
+            <Input label={`password`} type="password"{...register("password",{required:true, minLength:{
+                value:8,
+                message:"password must be more than 8 digits"
+            }})}/>
             <Button/>
             </form>
         </div>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,55 +1,73 @@
 import { ID } from "appwrite";
 import { Button, Input } from "./index";
-import { useForm } from "react-hook-form";
+import { set, useForm } from "react-hook-form";
 import { useNavigate } from "react-router";
 import authService from "../appwrite/Auth";
 import { useDispatch } from "react-redux";
 import { toggleLoginRedux } from "../store/AuthSlice";
+import { useState } from "react";
 
 export default function Signup() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [err, setErr] = useState("");
 
   async function submit(data) {
+    console.log("sb");
+
     try {
       await authService.createAccount({
         email: data.email,
         password: data.password,
         name: data.name,
-      })
-      const userData = await authService.getAccount()
+      });
+      const userData = await authService.getAccount();
       if (userData) {
         dispatch(toggleLoginRedux(userData));
         navigate("/");
       }
     } catch (error) {
-        console.log("/src/components/signup: ",error);
-        
+      setErr(error.message)
+      console.log("/src/components/signup: ", error);
     }
   }
 
   return (
     <div className="border rounded p-2 m-2 w-full">
+      {err && <p className="text-red-500" >{err}</p>}
       <form
         onSubmit={handleSubmit(submit)}
         className="flex gap-10 py-2 px-1 flex-col"
       >
+        {errors.name && <p className="text-red-500">{errors.name.message}</p> }
         <Input
           label={`name`}
-          {...register("name", { required: true, pattern: /^[A-Za-z]+$/i })}
+          {...register("name", {
+            required: true,
+            pattern: {
+              value:/^[A-Za-z]+$/i,
+              message: "Invalid name",
+            },
+          })}
         />
 
-        <Input
-          label={`username`}
-          {...register("email", { required: true })}
-        />
+{errors.email && <p className="text-red-500 ">{errors.email.message}</p> }
+        <Input label={`username`} {...register("email", { required: true,pattern:{
+          value:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+          message: "Invalid email format"
+        } })} />
 
+        {errors.password && <p className="text-red-500" >{errors.password.message}</p> }
         <Input
           label={`password`}
-          {...register("password", { required: true, minLength: 8 })}
-        />
-        <Button/>
+          {...register("password", { required: true, minLength: {value:8,message: "password must be more than 8 digits"} })}
+          />
+        <Button />
       </form>
     </div>
   );
