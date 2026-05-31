2a43ec8b4f68a36c2885712d2481fa656e04d615
Abhishek <abhishek29112005@gmail.com>
2026-05-31 08:55:08 +0530

    fix home rendering

--- src/main.jsx
+++ src/main.jsx
@@ -21,7 +21,11 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: "home",
+        path:"/home",
+        element:<HomeP/>
+      },
+      {
+        index: true,
         element: (
             <HomeP />
         ),
