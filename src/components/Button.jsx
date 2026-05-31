ee0c19233648bd638f6117ace24e60cc1091c983
Abhishek <abhishek29112005@gmail.com>
2026-05-31 13:55:27 +0530

    update ui

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,7 +8,7 @@ export default function Button({
   return (
     <button
       type={type}
-      className={`${clssName} bg-blue-500 font-medium rounded w-full`}
+      className={`${clssName} bg-blue-500 font-medium rounded w-full active:bg-blue-400 hover:border`}
       {...props}
       onClick={onclick}
     >
