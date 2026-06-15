e421468163fd2313e7513541e3e757c294fbe86d
Abhishek <abhishek29112005@gmail.com>
2026-06-15 17:06:05 +0530

    unauth routing fix

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,10 +1,18 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 
 export default function AuthLayout({ children }) {
-  const navigate = useNavigate()
-        const loggedIn = useSelector((state) => state.auth.status);
-  if (!loggedIn) {
-    return navigate("/login")
-  } else return children;
+  const navigate = useNavigate();
+  const loggedIn = useSelector((state) => state.auth.status);
+
+  useEffect(() => {
+    if (!loggedIn) {
+      navigate("/login");
+    } else;
+  }, [loggedIn]);
+
+
+  loggedIn? children : null
+
 }
